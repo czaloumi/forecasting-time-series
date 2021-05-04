e701550e5fbb45511a6929e0b94a5cd988b4403e
Chelsea Zaloumis <chelseazaloumis@Chelseas-MacBook-Pro.local>
2021-05-04 13:03:27 -0700

    data loading pipeline

---
+++ src/data_pipeline.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import random
+import datetime
+from helper_functions import *
+
+def load_separate_datasets():
+    train_sales = pd.read_csv("../data/sales_train_evaluation.csv")
+    calendar = pd.read_csv("../data/calendar.csv")
+    sell_prices = pd.read_csv("../data/sell_prices.csv")
+    return train_sales, calendar, sell_prices
+
+def clean_melt_and_merge(train_sales, calendar, sell_prices):
+    to_drop = ['event_name_1', 'event_type_1', 'event_name_2', 'event_type_2']
+    calendar.drop(to_drop, axis=1, inplace=True)
+
+    train_sales_melt = pd.melt(train_sales, 
+            id_vars=['id', 'item_id', 'dept_id', 'cat_id', 'store_id', 'state_id'],
+            var_name='d', value_name='item_sales')
+
+    df = pd.merge(train_sales_melt, calendar, on='d')
+
+    to_drop = ['weekday', 'wday', 'month', 'year']
+    df.drop(to_drop, axis=1, inplace=True)
+
+    final = df.merge(sell_prices, on=['item_id', 'store_id', 'wm_yr_wk'], how='left')
+    return final
+
+def save(df):
+    df.to_csv('../data/final_df.csv')
+
+if __name__ == "__main__":
+    train_sales, calendar, sell_prices = load_separate_datasets()
+
+    df = clean_melt_and_merge(train_sales, calendar, sell_prices)
+    print(df.head())
+
+    save(df)
